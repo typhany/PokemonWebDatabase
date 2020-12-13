6e096bdb73658c79490a43faacda07c184059abe
Tiffany Kong <tiffy.wildstar@gmail.com>
2020-12-12 17:47:31 -0800

    updated home/search pages and edited pokemon_page

--- home_page.js
+++ home_page.js
@@ -1,42 +1,8 @@
-// const P = new Pokedex.Pokedex();
-
-// async function getAllPokemons(){
-	
-//     var interval = {offset: 0, limit: 151};
-//     const response = await P.getPokemonsList(interval);
-//     console.log(response);
-//     for(var i in response.results){
-//         createPokemonTableCells(response.results[i].name);
-//     }
-
-// }
-
-// async function createPokemonTableCells(pokemon){
-
-// 	const response = await P.getPokemonByName(pokemon);
-// 	var pokemon_name = response.name;
-// 	var pokemon_img = response.sprites.front_default;
-// 	var pokemon_id = response.id;
-
-// 	$("#pokemon_table").append("<tr><td>" + 
-// 	                        "<img src = " + pokemon_img + " alt = " + pokemon_name +  "></img><br>" +
-// 	                        "#" + pokemon_id + " " + pokemon_name + "</td></tr>");
-
-
-// }
-
-
-// $(document).ready(function() {
-
-// 	getAllPokemons();
-
-// });
-
-
 function getAllPokemon(){
 	fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
 		.then(response => response.json())
 		.then(function(pokemon_list) {
+            console.log(pokemon_list.results);
 			for (var i in pokemon_list.results)
 			{
 				getPokemonData(pokemon_list.results[i]);
@@ -52,15 +18,22 @@ function getPokemonData(pokemon){
 			var pokemon_name = pokemon_info.name;
 			var pokemon_sprite = pokemon_info.sprites.front_default;
 
-			$("#pokemon_table").append("<button>" + //add function to get to pokmeon_page.html laters
-				                        "<img src = " + pokemon_sprite + " alt = " + pokemon_name +  "></img><br>" +
-				                        "#" + pokemon_id + " " + pokemon_name + "</button>");
+			$("#pokemon_table").append( "<a href = 'pokemon_page.html' >" + 
+				"<button type = 'submit' onclick = 'accessPokemonPage( " + pokemon_id + " );' " +
+				" id = " + pokemon_id+ " value = " + pokemon_name + ">" +
+				"<img src = " + pokemon_sprite + " alt = " + pokemon_name +  "></img><br>" +
+				"#" + pokemon_id + " " + pokemon_name + "</button></a>");
 
-
-		
 		})
+}
+
+
+function accessPokemonPage(id){
+	localStorage.setItem(document.getElementById(id).value, document.getElementById(id).value);    
+
 }
 
 $(document).ready(function() {
 	getAllPokemon();
+
 });
--- search_page.js
+++ search_page.js
@@ -1,106 +1,78 @@
-
-const P = new Pokedex.Pokedex();
 var all_pokemon = [];
-var current_pokemon = null;
 
 
 function findPokemon(){
-    
-    $('#results tr').remove();
-    // $('#pokemon_buttons').remove();
     $('#error').empty();
-
-
-        P.getPokemonByName($('#searchpokemon').val()).then(response =>{
-            console.log(response);
-            
-            // var table_row_max = 3;
-            // for (let i = 0; i < table_row_max; i++)
-            // {
-            //     var btn = document.createElement("button");
-            //     var image_link = response.sprites.front_default;
-            //     var pokemon_name = document.createTextNode(response.name);
-            //     console.log(pokemon_name);
-            //     var pokemon_id = response.id;
-
-            //     // btn.value = pokemon_name;
-            //     // var btn2 = $('<button></button>').text(response.name);
-
-            //     btn.appendChild(pokemon_name);
-            //     console.log(btn);
-            //     var div = document.getElementById("pokemon_buttons");
-            //     console.log(div);
-            //     div.innerHTML += btn;
-            // }
-            
-            var image = "<img src = " + response.sprites.front_default + " alt = " + response.name+  "></img><br>";
-            //$('.results ').after( "<button type = button>" + image + response.name + " #" + response.id + " </button>");
-
-            $('#results tbody').after( "<tr><td>" + "<a href = 'pokemon_page.html'>" + 
-            "<button onclick = 'accessPokemonPage(' " + response.id + " );' " +
-             " id = " + response.id + " value = " +response.name + ">" + image +"</button><br></a>"+ response.name + " #" + response.id + " </td></tr>");
-            // var button_val = $('.' + response.name).text();
-        
-            // var current_pokemon = document.getElementById(response.id).value;
-            // console.log(val1);
-
-
-
-            // var btn = document.createElement('a');
-            // btn.href = "unknown.jpg"//response.sprites.front_default;
-            // var spanBtn = document.createElement('span');
-            // spanBtn.classList.add("buttonToPokemon");
-            // btn.appendChild(spanBtn);
-            // btn.insertAdjacentText('beforeend', "pokemon name goes here"); //response.name);
-            
-            // var buttonLocation = document.getElementById("pokemon_buttons");
-            // $('#pokemon_buttons').after(btn);
-            // buttonLocation.appendChild(btn);
-        })
-    
-        .catch( (err) =>{
-            $('#error').text( "Incorrect Search, Try Again");
-        });
+    $('#results').empty();
 
    
+    var counter = 0;
+    var pokemon = $('#searchpokemon').val();
+    if(pokemon == ''){
+        $('#error').text("Incorrect Search, Try Again");
+        return;
+    }
+
+    if(  parseInt(pokemon) - 1 in all_pokemon){
+        getPokemonByName(all_pokemon[ parseInt(pokemon) - 1]);
+        counter += 1;
+    }
+
+    for(var i in all_pokemon){
+ 
+        if( all_pokemon[i].name.includes(pokemon)){
+            getPokemonByName(all_pokemon[i]);
+            counter += 1;
+        }
+        
+    }
+    if(counter == 0){
+        $('#error').text("Incorrect Search, Try Again");
+    }
 }
 
 function accessPokemonPage(id){
-    var current_pokemon = document.getElementById(id).value;
-}
-
-
-function getAllPokemons(){
-    // var all_pokemon = [];
-    P.getGenerationByName("generation-i").then(response =>{
-        console.log(response.pokemon_species);
-        for(var i in response.pokemon_species){
-            all_pokemon.push(response.pokemon_species[i].name);
-        }
-        
-        console.log(all_pokemon);
-        // return all_pokemon;
-        
-    })
-    // console.log(all_pokemon);
+    localStorage.setItem(document.getElementById(id).value, document.getElementById(id).value);    
 }
 
 
 
 
+function getAllPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json())
+        .then(function(pokemon_list) {
+            for (var i in pokemon_list.results)
+            {
+                all_pokemon.push(pokemon_list.results[i]);
 
+            }
+        })    
+}
+
+function getPokemonByName(pokemon){
+    fetch(pokemon.url)
+        .then(response => response.json())
+        .then(function(pokemon_info) {
+            var pokemon_id = pokemon_info.id;
+            var pokemon_name = pokemon_info.name;
+            var pokemon_sprite = pokemon_info.sprites.front_default;
+            
+
+            var image = "<img src = " + pokemon_sprite + " alt = " + pokemon_name+  "></img>";
+
+            $('#results').append( "<a href = 'pokemon_page.html' >" + 
+            "<button type = 'submit' onclick = 'accessPokemonPage( " + pokemon_id + " );' " +
+             " id = " + pokemon_id+ " value = " + pokemon_name + ">" + image + "<br>" +
+             "#" + pokemon_id+ " " + pokemon_name+ "</button></a>");
+           
+        })    
+        
+}
 
 
 $(document).ready(function() {
-    // const options = {
-    //     protocol: "https",
-    //     cache: true,
-    //     timeout: 5 * 1000, // 5s
-    //     cacheImages: true
-    //   }
-    // const P = new Pokedex.Pokedex();
-    getAllPokemons();
-    console.log(all_pokemon);
-
+    getAllPokemon();
 
 });
+
